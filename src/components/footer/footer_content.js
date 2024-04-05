07277eb370fd66b467f3313b3502907db370c31b
Ajay Kumar Gond <60037249+ajayg51@users.noreply.github.com>
2024-04-05 22:48:37 +0530

    Update footer_content.js

--- src/components/footer/footer_content.js
+++ src/components/footer/footer_content.js
@@ -3,7 +3,7 @@ const FooterContent = {
     mobNoCleanText : "+91 8090485110",
     gmailID : "mailto:hi.ajay51@gmail.com",
     gmailIDCleanLink : "hi.ajay51@gmail.com",
-    resumeLink : "https://drive.google.com/file/d/1y5fCgmqK_3_k_rF-4-XAdyWQMPNTenJ7/view",
+    resumeLink : "https://drive.google.com/file/d/1Tewl6kIO3c5UI9OWT51lBTjMr6eQPgWi/view?usp=sharing",
 
 }
 
